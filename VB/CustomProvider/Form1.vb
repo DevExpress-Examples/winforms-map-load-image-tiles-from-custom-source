b9ed251544b6fe74de48d93d474e2997c6b4391d
DevExpressExampleBot <fakeEmail>
2022-08-17 15:38:17 +0400

    Source auto update [skip ci]

--- VB/CustomProvider/Form1.vb
+++ VB/CustomProvider/Form1.vb
@@ -1,68 +1,66 @@
-﻿Imports Microsoft.VisualBasic
 Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraMap
 Imports System.IO
 
 Namespace CustomProvider
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			' Create a map control, set its dock style and add it to the form.
-			Dim map As New MapControl()
-			map.Dock = DockStyle.Fill
-			Me.Controls.Add(map)
+    Public Partial Class Form1
+        Inherits Form
 
-			' Create a layer to load image tiles from a local map data provider.
-			Dim imageTilesLayer As New ImageTilesLayer()
-			map.Layers.Add(imageTilesLayer)
-			imageTilesLayer.DataProvider = New LocalProvider()
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		End Sub
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            ' Create a map control, set its dock style and add it to the form.
+            Dim map As MapControl = New MapControl()
+            map.Dock = DockStyle.Fill
+            Me.Controls.Add(map)
+            ' Create a layer to load image tiles from a local map data provider.
+            Dim imageTilesLayer As ImageTilesLayer = New ImageTilesLayer()
+            map.Layers.Add(imageTilesLayer)
+            imageTilesLayer.DataProvider = New LocalProvider()
+        End Sub
 
-		Public Class LocalProvider
-			Inherits MapDataProviderBase
-			Private ReadOnly projection_Renamed As New SphericalMercatorProjection()
+        Public Class LocalProvider
+            Inherits MapDataProviderBase
 
-			Public Sub New()
-				TileSource = New LocalTileSource(Me)
-			End Sub
+            Private ReadOnly projectionField As SphericalMercatorProjection = New SphericalMercatorProjection()
 
-			Public Overrides ReadOnly Property Projection() As IProjection
-				Get
-					Return projection_Renamed
-				End Get
-			End Property
-			Public Overrides Function GetMapSizeInPixels(ByVal zoomLevel As Double) As MapSize
-				Dim imageSize As Double
-				If zoomLevel < 1.0 Then
-					imageSize = zoomLevel * LocalTileSource.tileSize * 2
-				End If
-				imageSize = Math.Pow(2.0, zoomLevel) * LocalTileSource.tileSize
-				Return New MapSize(imageSize, imageSize)
-			End Function
-		End Class
+            Public Sub New()
+                TileSource = New LocalTileSource(Me)
+            End Sub
 
-		Public Class LocalTileSource
-			Inherits MapTileSourceBase
-			Public Const tileSize As Integer = 256
-			Public Const maxZoomLevel As Integer = 1
+            Public Overrides ReadOnly Property Projection As IProjection
+                Get
+                    Return projectionField
+                End Get
+            End Property
 
-			Public Sub New(ByVal cacheOptionsProvider As ICacheOptionsProvider)
-				MyBase.New(512, 512, 256, 256, cacheOptionsProvider)
-			End Sub
+            Public Overrides Function GetMapSizeInPixels(ByVal zoomLevel As Double) As MapSize
+                Dim imageSize As Double
+                If zoomLevel < 1.0 Then imageSize = zoomLevel * LocalTileSource.tileSize * 2
+                imageSize = Math.Pow(2.0, zoomLevel) * LocalTileSource.tileSize
+                Return New MapSize(imageSize, imageSize)
+            End Function
+        End Class
 
-			Public Overrides Function GetTileByZoomLevel(ByVal zoomLevel As Integer, ByVal tilePositionX As Integer, ByVal tilePositionY As Integer) As Uri
-				If zoomLevel = 1 Then
-					Return New Uri(String.Format("file://" & Directory.GetCurrentDirectory() & "\tile_{0}_{1}_{2}.png", zoomLevel, tilePositionX, tilePositionY))
-				End If
-				Return Nothing
-			End Function
-		End Class
+        Public Class LocalTileSource
+            Inherits MapTileSourceBase
 
-	End Class
+            Public Const tileSize As Integer = 256
+
+            Public Const maxZoomLevel As Integer = 1
+
+            Public Sub New(ByVal cacheOptionsProvider As ICacheOptionsProvider)
+                MyBase.New(512, 512, 256, 256, cacheOptionsProvider)
+            End Sub
+
+            Public Overrides Function GetTileByZoomLevel(ByVal zoomLevel As Integer, ByVal tilePositionX As Integer, ByVal tilePositionY As Integer) As Uri
+                If zoomLevel = 1 Then Return New Uri(String.Format("file://" & Directory.GetCurrentDirectory() & "\tile_{0}_{1}_{2}.png", zoomLevel, tilePositionX, tilePositionY))
+                Return Nothing
+            End Function
+        End Class
+    End Class
 End Namespace
