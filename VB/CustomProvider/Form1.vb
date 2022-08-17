d43ad32ae4d2ee6259099e6808e9d8d8ad6b6291
DevExpressExampleBot <fakeEmail>
2022-08-17 15:40:26 +0400

    Source auto update [skip ci]

--- VB/CustomProvider/Form1.vb
+++ VB/CustomProvider/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Windows.Forms
 Imports DevExpress.XtraMap
 Imports System.IO
@@ -6,39 +6,37 @@ Imports System.Drawing
 Imports System.Diagnostics
 
 Namespace CustomProvider
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
             ' Create a map control, set its dock style and add it to the form.
-            Dim map As New MapControl()
+            Dim map As MapControl = New MapControl()
             map.Dock = DockStyle.Fill
             Me.Controls.Add(map)
-
             ' Create a layer to load image tiles from a local map data provider.
-            Dim imageTilesLayer As New ImageTilesLayer()
+            Dim imageTilesLayer As ImageTilesLayer = New ImageTilesLayer()
             map.Layers.Add(imageTilesLayer)
             imageTilesLayer.DataProvider = New LocalProvider()
-
         End Sub
 
         Public Class LocalProvider
             Inherits MapDataProviderBase
 
-
-            Private ReadOnly projection_Renamed As New SphericalMercatorProjection()
+            Private ReadOnly projectionField As SphericalMercatorProjection = New SphericalMercatorProjection()
 
             Public Sub New()
                 TileSource = New LocalTileSource(Me)
             End Sub
 
-            Public Overrides ReadOnly Property Projection() As ProjectionBase
+            Public Overrides ReadOnly Property Projection As ProjectionBase
                 Get
-                    Return projection_Renamed
+                    Return projectionField
                 End Get
             End Property
 
@@ -47,7 +45,8 @@ Namespace CustomProvider
                 imageSize = LocalTileSource.CalculateTotalImageSize(zoomLevel)
                 Return New MapSize(imageSize, imageSize)
             End Function
-            Protected Overrides ReadOnly Property BaseSizeInPixels() As Size
+
+            Protected Overrides ReadOnly Property BaseSizeInPixels As Size
                 Get
                     Return New Size(Convert.ToInt32(LocalTileSource.tileSize * 2), Convert.ToInt32(LocalTileSource.tileSize * 2))
                 End Get
@@ -58,27 +57,28 @@ Namespace CustomProvider
             Inherits MapTileSourceBase
 
             Public Const tileSize As Integer = 256
+
             Public Const maxZoomLevel As Integer = 2
+
             Private directoryPath As String
 
             Friend Shared Function CalculateTotalImageSize(ByVal zoomLevel As Double) As Double
-                If zoomLevel < 1.0 Then
-                    Return zoomLevel * tileSize * 2
-                End If
+                If zoomLevel < 1.0 Then Return zoomLevel * tileSize * 2
                 Return Math.Pow(2.0, zoomLevel) * tileSize
             End Function
 
             Public Sub New(ByVal cacheOptionsProvider As ICacheOptionsProvider)
-                MyBase.New(CInt((CalculateTotalImageSize(maxZoomLevel))), CInt((CalculateTotalImageSize(maxZoomLevel))), tileSize, tileSize, cacheOptionsProvider)
-                Dim dir As New DirectoryInfo(Directory.GetCurrentDirectory())
+                MyBase.New(CInt(CalculateTotalImageSize(maxZoomLevel)), CInt(CalculateTotalImageSize(maxZoomLevel)), tileSize, tileSize, cacheOptionsProvider)
+                Dim dir As DirectoryInfo = New DirectoryInfo(Directory.GetCurrentDirectory())
                 directoryPath = dir.Parent.Parent.FullName
             End Sub
 
             Public Overrides Function GetTileByZoomLevel(ByVal zoomLevel As Integer, ByVal tilePositionX As Integer, ByVal tilePositionY As Integer) As Uri
                 If zoomLevel <= maxZoomLevel Then
-                    Dim u As New Uri(String.Format("file://" & directoryPath & "\openstreetmap.org\Hybrid_{0}_{1}_{2}.png", zoomLevel, tilePositionX, tilePositionY))
+                    Dim u As Uri = New Uri(String.Format("file://" & directoryPath & "\openstreetmap.org\Hybrid_{0}_{1}_{2}.png", zoomLevel, tilePositionX, tilePositionY))
                     Return u
                 End If
+
                 Return Nothing
             End Function
         End Class
